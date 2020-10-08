467207b56fd9fe135fcb53af374b44cdabed4431
dognmrt <46164888+dognmrt@users.noreply.github.com>
2020-10-08 14:11:45 +0300

    Initial commit

---
+++ app/src/main/java/com/dogannn/ceng3545/week1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dogannn.ceng3545.week1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
